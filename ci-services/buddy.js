a09e3474335291c0b5639ed7a7ba769abf8b59f2
Jon Thompson <jon@iu-hq.com>
2018-05-19 14:25:31 +0100

    fix(buddy): make firstPush, correctBuild, uploadBuild always true

--- ci-services/buddy.js
+++ ci-services/buddy.js
@@ -7,8 +7,7 @@ const env = process.env;
 module.exports = {
   repoSlug: env.BUDDY_REPO_SLUG,
   branchName: env.BUDDY_EXECUTION_BRANCH,
-  firstPush:
-    gitHelpers.getNumberOfCommitsOnBranch(env.BUDDY_EXECUTION_BRANCH) === 1,
-  correctBuild: env.BUDDY_EXECUTION_PULL_REQUEST_ID > 0,
+  firstPush: true,
+  correctBuild: true,
   uploadBuild: true,
 };
